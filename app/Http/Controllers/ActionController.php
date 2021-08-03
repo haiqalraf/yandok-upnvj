1f0ac5ffc31dcfcf9185f1de20238c8d1cd9242d
haiqalrama <haiqalramanizaralfajri@gmail.com>
2021-08-03 14:59:04 +0700

    fix ActionController.php

--- app/Http/Controllers/ActionController.php
+++ app/Http/Controllers/ActionController.php
@@ -4,56 +4,73 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Validator;
+use Image;
 
 class ActionController extends Controller
 {
     //
     public function updateUser(Request $request){
+        
+        $request->validate([
+            'nim' => 'required|string',
+            'handphone' => 'required|string',
+            'telepon' => 'required|string',
+            'alamat' => 'required|string',
+            'email' => 'required|email',
+        ]);
+            
+        try {
 
-        if ( $request->nim == null || $request->nim == '' ){
+            $data = [
+                'no_hp' => $request->handphone,
+                'no_rumah' => $request->telepon,
+                'address' => $request->alamat,
+                'email' => $request->email,
+            ];
+
+            if (!empty($request->password)){
+                $data['password'] = bcrypt($request->password);
+            };
+            
+            if ($request->hasFile('foto')) {
+
+                $image      = $request->file('foto');
+                $fileName   = $request->nim.'.' . $image->getClientOriginalExtension();
+                
+                // if (file_exists(public_path().'/img/user/'.$fileName)) {
+                //     unlink(public_path().'/img/user/'.$fileName);
+                // }
+    
+                $img = Image::make($image->getRealPath());
+                $img->resize(120, 120, function ($constraint) {
+                    $constraint->aspectRatio();                 
+                });
+    
+                $img->save(public_path('/img/user/' .$fileName)); // <-- Key point
+
+                $data['photo'] = $fileName;
+            }
+
+            $User = new User;
+            $User = $User->where('nim', $request->nim);
+            $User->update($data);
+
+            return response()->json([
+        
+                'status' => true,
+                'text' => 'xxxx.',
+    
+            ], 200);
+            
+        } catch (Exception $err) {
 
             return response()->json([
         
                 'status' => false,
-                'text' => 'NIM lo ga ada.',
+                'text' => 'Error: '.$err,
     
-            ], 400);
-
-        } else {
-            
-            try {
-
-                $data = [
-                    'email' => $request->email,
-                    'no_hp' => $request->handphone,
-                    'no_rumah' => $request->telepon,
-                    'pekerjaan' => $request->pekerjaan,
-                    'address' => $request->alamat,
-                ];
-                
-                $password = $request->password != '' ?  $data['password'] = bcrypt($request->password) : ''; 
-
-                $User = new User;
-                $User = $User->where('nim', $request->nim);
-                $User->update($data);
-
-                return response()->json([
-            
-                    'status' => false,
-                    'text' => 'ok.',
-        
-                ], 200);
-                
-            } catch (Exception $err) {
-
-                return response()->json([
-            
-                    'status' => false,
-                    'text' => 'Error: '.$err,
-        
-                ], 200);
-            }
-
+            ], 200);
         }
     }
 }
