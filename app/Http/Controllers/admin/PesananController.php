5be526fa0dc820bc296016f0b936fe5f9890c13e
haiqalrama <haiqalramanizaralfajri@gmail.com>
2021-10-30 08:32:40 +0700

    Memisahkan controller admin surat keterangan

--- app/Http/Controllers/admin/PesananController.php
+++ app/Http/Controllers/admin/PesananController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\admin;
 
 use App\Models\User;
-use App\Models\Suket;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -12,54 +11,6 @@ use Illuminate\Support\Facades\Storage;
 
 class PesananController extends Controller
 {
-    public function surat(Request $request)
-    {
-        $surat = [];
-        if ($request->has('status')) {
-            $surat = Suket::where('verifikasi', $request->status)->get();
-        } else {
-            $surat = Suket::where('verifikasi', '1')->get();
-        }
-        
-        return view('admin.surat.index', [
-            'surat' => $surat->sortByDesc('updated_at'),
-            'status' => $request->status
-        ]);
-    }
-
-    public function detailSurat(Suket $surat)
-    {
-        $daftar_pesanan = collect([]);
-        foreach ($surat->dokumen_dipesan as $values) {
-            foreach ($values as $value) {
-                $daftar_pesanan->put($value, 1);
-            }
-        }
-        
-        return view('admin.surat.detail', [
-            'surat' => $surat,
-            'daftar_pesanan' => $daftar_pesanan
-        ]);
-    }
-
-    public function updateSurat(Suket $surat, Request $request)
-    {
-        $request->validate([
-            'status' => 'required'
-        ]);
-
-        if ($request->status === '0') {
-            $surat->komentar = $request->komentar;
-        }
-
-        $surat->verifikasi = $request->status;
-        $surat->save();
-
-        $adminTitle = auth()->user()->adminTitle();
-
-        return redirect()->route($adminTitle.'.surat', ['status' => $request->status]);
-    }
-
     public function lainnya(Request $request)
     {
         $lainnya = [];
